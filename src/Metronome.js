91b1401736693bd583e4a82c7a783330c8edb54c
Crysisun <sunqingyu1@gmail.com>
2018-10-08 16:54:28 -0700

    Now this can click once.

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,19 +1,47 @@
 import React, {Component} from 'react';
 import './Metronome.css';
-
+import click1 from './click1.wav';
+import click2 from './click2.wav';
 
 class Metronome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           playing: false,
+           bpm: 100,
+
+        };
+        this.click1 = new Audio(click1);
+        this.click2 = new Audio(click2);
+    }
+
+    handleChangeBpm = event => {
+        const bpm = event.target.value;
+        this.setState({bpm});
+    }
+
+    startStop = () => {
+        this.click1.play();
+    }
+
     render() {
-        let bpm = 100;
-        let playing = false;
+        const {playing, bpm} = this.state;
 
         return (
             <div className="metronome">
                 <div className="bpm-slider">
                     <div>{bpm} BPM</div>
-                    <input type="range" min="60" max="240" value={bpm}/>
+                    <input
+                        type="range"
+                        min="60"
+                        max="240"
+                        value={bpm}
+                        onChange={this.handleChangeBpm}
+                    />
                 </div>
-                <button>{playing ? 'Stop' : 'Start'}</button>
+                <button onClick={this.startStop}>
+                    {playing ? 'Stop' : 'Start'}
+                </button>
             </div>
         );
     }
