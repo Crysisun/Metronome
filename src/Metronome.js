b7e66cc9a4c99ff0a8cc0e720cfab6898b91e21e
Crysisun <sunqingyu1@gmail.com>
2018-10-08 17:43:11 -0700

    When the bpm is changed, a new timer with the new bpm will be generated.

--- src/Metronome.js
+++ src/Metronome.js
@@ -9,7 +9,8 @@ class Metronome extends Component {
         this.state = {
            playing: false,
            bpm: 100,
-
+           count: 0,
+           beatsPerMeasure: 4,
         };
         this.click1 = new Audio(click1);
         this.click2 = new Audio(click2);
@@ -17,12 +18,56 @@ class Metronome extends Component {
 
     handleChangeBpm = event => {
         const bpm = event.target.value;
-        this.setState({bpm});
-    }
+        if (this.state.playing) {
+            clearInterval(this.timer);
+            this.timer = setInterval(
+                this.playClick,
+                (60 / bpm) * 1000
+            );
+            this.setState({
+                    count: 0,
+                    bpm,
+            })
+        } else {
+            this.setState({bpm});
+        }
+    };
 
     startStop = () => {
-        this.click1.play();
-    }
+        if (this.state.playing) {
+            //This will clear the timer and stop the click
+            clearInterval(this.timer);
+            this.setState({
+                playing: false,
+            });
+        } else {
+            //Starts the timer
+            this.timer = setInterval(
+                this.playClick,
+                (60 / this.state.bpm) * 1000
+            );
+            this.setState(
+                {
+                count: 0,
+                playing: true,
+                },
+                this.playClick
+            )
+        }
+    };
+
+    playClick = () => {
+        const{count, beatsPerMeasure} = this.state;
+        if (count % beatsPerMeasure === 0) {
+            this.click2.play();
+        } else {
+            this.click1.play();
+        }
+
+        this.setState(state => ({
+            count: (state.count + 1) % state.beatsPerMeasure
+        }))
+    };
 
     render() {
         const {playing, bpm} = this.state;
